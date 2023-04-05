63471aacce03c792725ffe7dd82d72136708d7ef
Arron <arron.su@relaistech.net>
2023-04-05 19:29:36 +0800

    fix: 🐛 alertPopup file path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { HashRouter } from 'react-router-dom';
 import { Router } from './routers/index';
 import { BgCanvas } from '@/layouts/bgCanvas/index';
-import AlertPopup from '@/components/global/alertPopup';
+import AlertPopup from '@/layouts/alertPopup/alertPopup';
 
 function App() {
 	return (
